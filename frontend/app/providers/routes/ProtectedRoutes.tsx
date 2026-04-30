3ca70a259429aab8e54fb04048906459fd199c41
krims31 <vegor405@gmail.com>
2026-04-30 18:57:40 +0300

    finish all main page

--- frontend/app/providers/routes/ProtectedRoutes.tsx
+++ frontend/app/providers/routes/ProtectedRoutes.tsx
@@ -1,8 +1,8 @@
+import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuthState } from '../../../features/auth/state'
-
 type Props = {
-	children: JSX.Element
+	children: React.ReactNode
 }
 
 export const ProtectedRoutes = ({ children }: Props) => {
