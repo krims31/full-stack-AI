030301e4cd55246e2c4a008b09a51a0f6a7f37eb
krims31 <vegor405@gmail.com>
2026-05-28 18:20:58 +0300

    fix middleware

--- backend/shared/middleware/auth.middleware.ts
+++ backend/shared/middleware/auth.middleware.ts
@@ -14,7 +14,7 @@ export const authMiddleware = (
 	try {
 		const token = authHeader.split(' ')[1]
 
-		const decoded = jwt.verify(token, 'secret')
+		const decoded = jwt.verify(token, process.env.JWT_SECRET!)
 
 		req.user = decoded
 
