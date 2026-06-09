bf29083fc55c1089f96e1c6ff2c9bc6a5e9f7b1a
krims31 <vegor405@gmail.com>
2026-06-09 14:59:11 +0300

    add AllDone.tsx and fix him, and Add the first test in component

--- frontend/pages/ForgotPassword/AllDone/AllDone.tsx
+++ frontend/pages/ForgotPassword/AllDone/AllDone.tsx
@@ -1,3 +1,64 @@
+import { ArrowLeft, CircleCheckBig } from 'lucide-react'
+import { Link } from 'react-router'
+
 export default function AllDone() {
-	return <div>All-done</div>
+	return (
+		<>
+			<main>
+				<div className="border rounded-xl w-110 h-130 relative top-40 left-150 placeholder:text-gray-600 transition-all border-black/10 shadow-lg hover:shadow-xl outline-0 text-black">
+					<div className="border rounded-md w-10 h-10 relative top-20 left-52 placeholder:text-gray-600 transition-all border-black/10 shadow-lg hover:shadow-xl outline-0">
+						<CircleCheckBig className="mt-2 ml-2" />
+					</div>
+					<h1 className="flex item-center justify-center mt-25 ml-5 text-2xl text-black h-screen">
+						All done!
+					</h1>
+
+					<p className="flex item-center justify-center -mt-196 ml-0 gap-1 text-gray-600">
+						Your password has been reset. Would you like to <br /> set up Face
+						ID as well?
+					</p>
+
+					<div>
+						<button className="flex item-center justify-center border rounded-md w-80 h-10 transition-all border-black/10 bg-blue-600 shadow-lg hover:shadow-xl outline-0 cursor-pointer duration-300 hover:scale-105 active:scale-95 relative top-5 left-15">
+							<span className="mt-2 text-white">Set up Face ID</span>
+						</button>
+					</div>
+
+					<div>
+						<button className="flex item-center justify-center border rounded-md w-80 h-10 transition-all border-black/10 bg-whit shadow-lg hover:shadow-xl outline-0 cursor-pointer duration-300 hover:scale-105 active:scale-95 relative top-7 left-15">
+							<span className="mt-2 text-black">I'll do this later</span>
+						</button>
+					</div>
+
+					<div>
+						<Link
+							to="/login"
+							className="relative top-5 z-10 left-50 text-gray-600 text-sm"
+						>
+							<ArrowLeft className="relative z-10 top-6 right-7" />
+							Back to log in{' '}
+						</Link>
+					</div>
+				</div>
+			</main>
+
+			<footer>
+				<Link to="/forgotten-password">
+					<div className="border rounded-2xl w-20 h-3 relative top-60 left-155 hover:bg-blue-500 bg-gray-200 placeholder:text-gray-600 transition-all border-black/10 shadow-lg hover:shadow-xl outline-0 z-10"></div>
+				</Link>
+
+				<Link to="/password-reset">
+					<div className="border rounded-2xl w-20 h-3 relative top-57 left-180 hover:bg-blue-500 bg-gray-200 placeholder:text-gray-600 transition-all border-black/10 shadow-lg hover:shadow-xl outline-0 z-10"></div>
+				</Link>
+
+				<Link to="/set-new-password">
+					<div className="border rounded-2xl w-20 h-3 relative top-54 left-205 hover:bg-blue-500 bg-gray-200 placeholder:text-gray-600 transition-all border-black/10 shadow-lg hover:shadow-xl outline-0 z-10"></div>
+				</Link>
+
+				<Link to="/all-done">
+					<div className="border rounded-2xl w-20 h-3 relative top-51 left-230 hover:bg-blue-500 bg-gray-200 placeholder:text-gray-600 transition-all border-black/10 shadow-lg hover:shadow-xl outline-0 z-10"></div>
+				</Link>
+			</footer>
+		</>
+	)
 }
---
+++ frontend/pages/test/AllDone.test.tsx
@@ -0,0 +1,58 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { describe, expect, it } from 'vitest'
+import AllDone from '../ForgotPassword/AllDone/AllDone'
+
+describe('AllDone Component', () => {
+	const renderComponent = () =>
+		render(
+			<MemoryRouter>
+				<AllDone />
+			</MemoryRouter>
+		)
+
+	it('should render reset password', () => {
+		renderComponent()
+
+		const heading = screen.getByRole('heading', {
+			name: /all done!/i
+		})
+
+		expect(heading).toBeInTheDocument()
+	})
+
+	it('should render description', () => {
+		renderComponent()
+
+		const description = screen.getByText(/your password has been reset/i)
+
+		expect(description).toBeInTheDocument()
+	})
+
+	it('contains face id buttons', () => {
+		renderComponent()
+
+		expect(
+			screen.getByRole('button', {
+				name: /set up face id/i
+			})
+		).toBeInTheDocument()
+
+		expect(
+			screen.getByRole('button', {
+				name: /i'll do this later/i
+			})
+		).toBeInTheDocument()
+	})
+
+	it('contains navigation links', () => {
+		renderComponent()
+
+		const loginLink = screen.getByRole('link', {
+			name: /back to log in/i
+		})
+
+		expect(loginLink).toHaveAttribute('href', '/login')
+	})
+})
