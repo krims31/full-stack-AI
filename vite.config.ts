197c36bc56f83455a902017b04528eb7852075a9
krims31 <vegor405@gmail.com>
2026-06-09 15:10:19 +0300

    add test

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
-// https://vite.dev/config/
+// https://vite.dev/config/ 
+
 export default defineConfig({
 	plugins: [
 		react(),
@@ -15,5 +16,11 @@ export default defineConfig({
 		alias: {
 			'@': path.resolve(__dirname, './src')
 		}
+	},
+
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./frontend/setupTest.ts']
 	}
 })
